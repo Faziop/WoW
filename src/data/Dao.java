9bf942770404b81f50ca540135c72ded1951ea50
Unknown <alessandro.fazio.perez@gmail.com>
2019-04-17 17:34:43 -0600

    lista de jugadores

--- src/data/Dao.java
+++ src/data/Dao.java
@@ -31,28 +31,34 @@ public class Dao {
         String sql = "insert into Jugador (nombre, genero, color_de_piel, nivel, raza, clase, faccion, ubicacion) "
                 + "values ('%s', '%s', '%s', %d, %d, %d, %d, %d)";
         sql = String.format(sql, j.getNombre(), j.getGenero(), j.getColor(), j.getNivel(), j.getRaza().getId(), j.getClase().getId(), j.getFaccion().getId(), 1);
+        try {
+            db.connect();
+            if (db.executeUpdate(sql) == 0) {
+                throw new Exception("Jugador ya existe");
+            }
 
-        if (db.executeUpdate(sql) == 0) {
-            throw new Exception("Jugador ya existe");
+        } finally {
+            db.disconnect();
         }
     }
 
     public ArrayList<Jugador> listaJugadores() throws Exception {
-        String sql = "select jugador.nombre, jugador.genero, jugador.color_de_piel, jugador.nivel, jugador.raza, jugador.clase, jugador.faccion, jugador.ubicacion, "
-                + "clase.id, clase.nombre, raza.id, raza.nombre, raza.region, faccion.id, faccion.nombre "
-                + "from Jugador, Clase, Raza, Faccion "
-                + "where Jugador.raza = Raza.id and Jugador.clase = Clase.id and Jugador.faccion = Faccion.id";
-        ResultSet rs = db.executeQuery(sql);
+        String sql = "select jugador.nombre, jugador.genero, jugador.color_de_piel, jugador.nivel, jugador.raza, jugador.clase, jugador.faccion, jugador.ubicacion, clase.id, clase.nombre, raza.nombre, faccion.nombre, sitio.nombre from Jugador, Clase, Raza, Faccion, Sitio where Jugador.raza = Raza.id and Jugador.clase = Clase.id and Jugador.faccion = Faccion.id";
         ArrayList<Jugador> jugadores = new ArrayList<>();
 
-        while (rs.next()) {
-            //Ocupo aiuda
-//            jugadores.add(new Jugador(rs.getString("nombre"), rs.getString("genero"), rs.getString("color_de_piel"), rs.getInt("nivel"), 
-//                    new Clase(rs.getInt("id"), rs.getString("nombre:1")),
-//                    new Raza(rs.getInt("id:1"), rs.getString("nombre:2")), 
-//                    new Faccion(rs.getInt("id:2"), rs.getString("nombre:3")), 
-//                    new Sitio(rs.getInt("id:3"), rs.getString("nombre:4"), rs.getString("tipo"), 
-//                            new Region(rs.getInt("region:1")))));
+        try {
+            db.connect();
+            ResultSet rs = db.executeQuery(sql);
+
+            while (rs.next()) {
+                jugadores.add(new Jugador(rs.getString("nombre"), rs.getString("genero"), rs.getString("color_de_piel"), rs.getInt("nivel"),
+                        new Clase(-1, rs.getString(9)),
+                        new Raza(-1, rs.getString(10)),
+                        new Faccion(-1, rs.getString(11)),
+                        new Sitio(-1, rs.getString(12), null, null), true));
+            }
+        } finally {
+            db.disconnect();
         }
 
         return jugadores;
